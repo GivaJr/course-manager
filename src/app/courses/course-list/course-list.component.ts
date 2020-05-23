c1ff9daa943e6b017f4f3ddfc11d837ffc925668
Givanildo Junior <givanildo.alfeu@ufrpe.br>
2020-05-23 16:32:38 -0300

    "starting app in repository "

---
+++ src/app/courses/course-list/course-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: 'course-list.component.html'
+
+})
+
+
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+
+  ngOnInit(): void {
+
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular Forms',
+        imagemURL: './assets/images/forms.png',
+        price: 150,
+        code: '123',
+        duration: 190,
+        rating: 5,
+        realeseDate: '12/12/1990'
+
+      },
+      {
+        id: 2,
+        name: 'Angular HTTP',
+        imagemURL: './assets/images/http.png',
+        price: 100,
+        code: '123',
+        duration: 100,
+        rating: 4.9,
+        realeseDate: ' 09/08/1995'
+
+      }
+    ];
+  }
+}
---
+++ src/app/courses/course-list/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+  id: number;
+  name: string;
+  imagemURL: string;
+  price: number;
+  code: string;
+  duration: number;
+  rating: number;
+  realeseDate: string;
+}
