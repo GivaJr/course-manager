6b86c427d51e66bae9f9881af7affd3d30fd4da5
Givanildo Junior <givanildo.alfeu@ufrpe.br>
2020-06-05 21:30:20 -0300

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import {StarComponent} from './courses/star/star.component';
+import { ReplacePipe } from './courses/pipe/replace.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
